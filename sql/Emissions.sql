d3ed32478041a976ff0626ad6403127dacbbb211
Sammam Sohail <65643349+Somewhat-Coder@users.noreply.github.com>
2024-04-21 04:13:09 +0100

    revised join of datasets

--- sql/Emissions.sql
+++ sql/Emissions.sql
@@ -31,3 +31,34 @@ primary key (underhood_id, stnd)
 /* To delete all data from both tables
 */
 truncate emission_standard cascade;
+
+
+
+-- Create extension and foreign server
+CREATE EXTENSION IF NOT exists postgres_fdw;
+CREATE SERVER foreign_server
+FOREIGN DATA WRAPPER postgres_fdw
+OPTIONS (host 'database-3.c5go4e6kame4.us-east-1.rds.amazonaws.com', dbname 'postgres', port '5432');
+
+-- Create user mapping
+CREATE USER MAPPING FOR postgres
+SERVER foreign_server
+OPTIONS (user 'postgres', password 'javamylife');
+
+-- Import foreign schema
+IMPORT FOREIGN SCHEMA public
+FROM SERVER foreign_server
+INTO public;
+
+SELECT *
+FROM 
+    (SELECT *
+     FROM car_details
+     INNER JOIN emission_standard ON car_details.stnd = emission_standard.stnd) AS details
+INNER JOIN 
+    (SELECT *
+     FROM electric_vehicles
+     INNER JOIN counties ON electric_vehicles.county_id = counties.id) AS population
+ON details.cert_region = population.state;
+
+DROP FOREIGN TABLE public.counties, public.electric_vehicles , public.vehicle_primary_uses;
